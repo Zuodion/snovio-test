2fce86480f9b15bf277878754e65d75ecef06ef1
Aleks Ivanov <alex.ivanov@smplfy.eu>
2020-12-16 11:27:10 +0200

    Init

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const { join } = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+const { HotModuleReplacementPlugin } = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: join(__dirname + '/src', 'main.js'),
+  output: {
+    path: join(__dirname, 'build'),
+    filename: 'main.min.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }, {
+        test: /.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new HotModuleReplacementPlugin(),
+    new VueLoaderPlugin(),
+    new HTMLWebpackPlugin({
+      showErrors: true,
+      cache: true,
+      template: join(__dirname + '/public', 'index.html')
+    })
+  ]
+};
